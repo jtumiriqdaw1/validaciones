9ed12bd1a4c6429065cf8087fb9fdffdc5fee83c
Karmele Arocena <dweskarmele2@gmail.com>
2021-04-22 12:58:34 +0200

    termina

--- helper/funciones.php
+++ helper/funciones.php
@@ -1,6 +1,6 @@
 <?php
 
-// La función verArray va a servir para ver al final las reglas y los errores
+// La función verArray() va a servir para ver al final las reglas y los errores
 function verArray($array)
 {
     echo "<pre>";
@@ -8,13 +8,14 @@ function verArray($array)
     echo "</pre>";
 }
 
+//COMPLETA la Función validar()
 function validar(---------------)
 {
-    //Se crea el array $errores
+    // Se crea el array $errores
     // Se recorre el array de reglas y el array de 
 
-    // si el campo es un elemento botón, casilla o elemento de lista sin seleccionar
-    // es decir no está isset y daría error: Hay que tratarlo
+    // Si el campo es un elemento botón, casilla o elemento de lista sin seleccionar,
+    // es decir no está isset, daría error: Hay que tratarlo
         unset($valor); // Para limpiar la variable en cada recorrido
        
 
